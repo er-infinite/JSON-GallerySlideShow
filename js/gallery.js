4ef31a41aec83e2c53dd354cdabb7696466fdc0e
er-infinite <elr9@njit.edu>
2019-04-08 13:54:56 -0400

    update XMLHttpRequest and add gallery images into mImages array from mJSON file

--- js/gallery.js
+++ js/gallery.js
@@ -43,41 +43,113 @@ function swapPhoto() {
 // Counter for the mImages array
 var mCurrentIndex = 0;
 
-//--- LOADING JSON FILE ---
+//--- Part 2: XMLHttp Request & Creating JSON object ---
+
+//Store location of JSON file in mURL
+const mURL = "images.json";
 
 //Create new XMLHTTPRequest object
 const mRequest = new XMLHttpRequest();
 
 //load XMLHTTPRequest object
-mRequest.onload = function(){
+mRequest.onreadystatechange = function(){
 
   //check if request is OK
-  if(this.status === 200){
+  if(mRequest.readyState == 4 && mRequest.status == 200){
     //try to parse response object
     try{
       //xhr.responseText is parsed to create into a javascript object
-      const resObj = JSON.parse(this.responseText);
-      console.log(resObj);
-    }catch (e){
-      console.warn('Error in the JSON. Could not parse');
+      mJson = JSON.parse(mRequest.responseText);
+      console.log(mJson);
+
+      //For each item in mJson, Loop through and push each GalleryImage element into mImages array
+      for(var i = 0; i < mJson.images.length; i++){
+
+          mImages.push(new GalleryImage (mJson.images[i].imgLocation, mJson.images[i].description, mJson.images[i].date, mJson.images[i].imgPath));
+
+      }
+      console.log("MJSON LENGTH: " + mJson.images.length);
+      // mImages.push(new GalleryImage mJson);
+      console.table(mImages);
+
+    }catch (err){
+      console.log(err.message);
     }
-
-    // console.log(this.responseText)
-  }else{
-    console.warn('Request not successful');
   }
-
 };
 
+
 //setup request and get a response
-mRequest.open('GET', 'images.json')
+mRequest.open('GET', mURL, true)
 mRequest.send();
 
+// //Create new XMLHTTPRequest object
+// const mRequest = new XMLHttpRequest();
+//
+// //load XMLHTTPRequest object
+// mRequest.onload = function(){
+//
+//   //check if request is OK
+//   if(this.status === 200){
+//     //try to parse response object
+//     try{
+//       //xhr.responseText is parsed to create into a javascript object
+//       const resObj = JSON.parse(this.responseText);
+//       console.log(resObj);
+//
+//     }catch (e){
+//       console.warn('Error in the JSON. Could not parse');
+//     }
+//
+//     // console.log(this.responseText)
+//   }else{
+//     console.warn('Request not successful');
+//   }
+// };
+//
+//
+// //setup request and get a response
+// mRequest.open('GET', 'images.json')
+// mRequest.send();
+
+// function loadImages(){
+//   const request = new XMLHttpRequest();
+//
+//   request.open('GET', 'images.json');
+//   request.onload = () => {
+//
+//     try{
+//       const mJson = JSON.parse(request.responseText)
+//       populateRankings(mJson);
+//     }catch (e){
+//       console.warn('Error in the JSON. Could not parse')
+//     }
+//   };
+//
+//   request.send();
+// }
+//
+// function populateRankings(json){
+//   console.log(json);
+// }
+//
+// loadImages();
+//
+
+//--- Iterate through JSON object ---
 // Array holding GalleryImage objects (see below).
 var mImages = [];
 
+
+var img = new Image();
+//log quantity of images in document
+console.log('After: ' + document.images.length);
+
+// img.src = 'url';
+
 // Holds the retrived JSON information
 var mJson;
+
 
 // URL for the JSON to load by default
 // Some options for you are: images.json, images.short.json; you will need to create your own extra.json later
@@ -98,6 +170,7 @@ $(document).ready( function() {
 	// This initially hides the photos' metadata information
 	$('.details').eq(0).hide();
 
+
 });
 
 window.addEventListener('load', function() {
@@ -109,13 +182,8 @@ window.addEventListener('load', function() {
 //Photo object is created with the following properties: location, description, date, and source
 function GalleryImage(imgLocation, imgInfo, imgDate, imgURL) {
   this.imgLocation = imgLocation;
-  this.imgDescription = imgInfo;
-  this.imgDate = imgDate;
-  this.imgURL = imgURL;
+  this.description = imgInfo;
+  this.date = imgDate;
+  this.imgPath = imgURL;
 
 }
-
-
-
-
-//Create a JSON object that contains the retrieved JSON3 string (in this case, a list of photo URLsand related metadata).
