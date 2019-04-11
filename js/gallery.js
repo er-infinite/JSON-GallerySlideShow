9c8cf9cecff5c3fe94f7933b7d9ef9e403bdc8a4
er-infinite <elr9@njit.edu>
2019-04-11 01:25:42 -0400

    fix get json file function

--- js/gallery.js
+++ js/gallery.js
@@ -1,14 +1,14 @@
 // requestAnim shim layer by Paul Irish
-    window.requestAnimFrame = (function(){
-      return  window.requestAnimationFrame       ||
-              window.webkitRequestAnimationFrame ||
-              window.mozRequestAnimationFrame    ||
-              window.oRequestAnimationFrame      ||
-              window.msRequestAnimationFrame     ||
-              function(/* function */ callback, /* DOMElement */ element){
-                window.setTimeout(callback, 1000 / 60);
-              };
-    })();
+window.requestAnimFrame = (function() {
+  return window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function( /* function */ callback, /* DOMElement */ element) {
+      window.setTimeout(callback, 1000 / 60);
+    };
+})();
 
 
 // example code from mr doob : http://mrdoob.com/lab/javascript/requestanimationframe/
@@ -18,16 +18,16 @@ animate();
 var mLastFrameTime = 0;
 var mWaitTime = 5000; //time in ms
 function animate() {
-    requestAnimFrame( animate );
-	var currentTime = new Date().getTime();
-	if (mLastFrameTime === 0) {
-		mLastFrameTime = currentTime;
-	}
+  requestAnimFrame(animate);
+  var currentTime = new Date().getTime();
+  if (mLastFrameTime === 0) {
+    mLastFrameTime = currentTime;
+  }
 
-	if ((currentTime - mLastFrameTime) > mWaitTime) {
-		swapPhoto();
-		mLastFrameTime = currentTime;
-	}
+  if ((currentTime - mLastFrameTime) > mWaitTime) {
+    swapPhoto();
+    mLastFrameTime = currentTime;
+  }
 }
 
 /************* DO NOT TOUCH CODE ABOVE THIS LINE ***************/
@@ -37,63 +37,76 @@ var mCurrentIndex = 0;
 
 function swapPhoto() {
 
-  if(mCurrentIndex < 0){
+  // if index is less than 0 (-1 = last photo), add length of images to get correct index number
+  if (mCurrentIndex < 0) {
     mCurrentIndex += mImages.length;
-    // $('#photo').attr('src', mImages[mCurrentIndex].imgPath);
   }
+
   //if current index is less than the number of images in the array
   // do the following
-  if (mCurrentIndex < mImages.length ){
+  if (mCurrentIndex < mImages.length) {
     //select photo div, change image to corresponding index number
     $('#photo').attr('src', mImages[mCurrentIndex].imgPath);
     console.log(mCurrentIndex);
     //increase index number
-    mCurrentIndex ++;
+    mCurrentIndex++;
     //else if index reaches end of gallery, restart slide show to first image
-  }else{
+  } else {
     mCurrentIndex = 0;
   }
-
-
-
-
 }
 
-
-
-//--- Part 2: XMLHttp Request & Creating JSON object ---
-
-//Store location of JSON file in mURL
-// const mURL = "images.json";
 
 // URL for the JSON to load by default
 // Some options for you are: images.json, images.short.json; you will need to create your own extra.json later
 var mUrl = 'images.json';
 
+function getQueryParams(qs) {
+  qs = qs.split("+").join(" ");
+  var params = {},
+   tokens,
+   re = /[?&]?([^=]+)=([^&]*)/g;
+   while (tokens = re.exec(qs)) {
+     params[decodeURIComponent(tokens[1])]
+     = decodeURIComponent(tokens[2]);
+   }
+   return params;
+ }
+
+var $_GET = getQueryParams(document.location.search);
+console.log($_GET["json"]); // would output "John"
+
+if ($_GET["json"] != undefined){
+  mUrl = 'images.json'
+}else{
+  $_GET["json"];
+}
+
+
 //Create new XMLHTTPRequest object
 const mRequest = new XMLHttpRequest();
 
 //load XMLHTTPRequest object
-mRequest.onreadystatechange = function(){
+mRequest.onreadystatechange = function() {
 
   //check if request is OK
-  if(mRequest.readyState == 4 && mRequest.status == 200){
+  if (mRequest.readyState == 4 && mRequest.status == 200) {
     //try to parse response object
-    try{
+    try {
       //xhr.responseText is parsed to create into a javascript object
       mJson = JSON.parse(mRequest.responseText);
       console.log(mJson);
 
       //For each item in mJson, Loop through and push each GalleryImage element into mImages array
-      for(var i = 0; i < mJson.images.length; i++){
-          //push gallery image properties (location, description, date, path) into mImages array
-          mImages.push(new GalleryImage (mJson.images[i].imgLocation, mJson.images[i].description, mJson.images[i].date, mJson.images[i].imgPath));
+      for (var i = 0; i < mJson.images.length; i++) {
+        //push gallery image properties (location, description, date, path) into mImages array
+        mImages.push(new GalleryImage(mJson.images[i].imgLocation, mJson.images[i].description, mJson.images[i].date, mJson.images[i].imgPath));
       }
       console.log("MJSON LENGTH: " + mJson.images.length);
       // mImages.push(new GalleryImage mJson);
       console.table(mImages);
 
-    }catch (err){
+    } catch (err) {
       console.log(err.message);
     }
   }
@@ -114,31 +127,31 @@ var mJson;
 //You can optionally use the following function as your event callback for loading the source of Images from your json data (for HTMLImageObject).
 //@param A GalleryImage object. Use this method for an event handler for loading a gallery Image object (optional).
 function makeGalleryImageOnloadCallback(galleryImage) {
-	return function(e) {
-		galleryImage.img = e.target;
-		mImages.push(galleryImage);
-	}
+  return function(e) {
+    galleryImage.img = e.target;
+    mImages.push(galleryImage);
+  }
 }
 
-$(document).ready( function() {
+$(document).ready(function() {
 
-	// This initially hides the photos' metadata information
-	$('.details').eq(0).hide();
+  // This initially hides the photos' metadata information
+  $('.details').eq(0).hide();
 
   //rotate moreIndicator and display details when clicked
-  $('.moreIndicator').on('click', () =>{
+  $('.moreIndicator').on('click', () => {
     // $('.moreIndicator').addClass("rot270").hasClass("rot90");
     // $('.moreIndicator').removeClass("rot270").addClass("rot90");
-  $('.moreIndicator').toggleClass('rot90 rot270');
-  $("div.details").fadeToggle();
-  console.log('mouse click');
+    $('.moreIndicator').toggleClass('rot90 rot270');
+    $("div.details").fadeToggle();
+    console.log('mouse click');
   });
 
-  $("#nextPhoto").on('click', ()=>{
+  $("#nextPhoto").on('click', () => {
     swapPhoto();
   })
 
-$("#prevPhoto").on('click', ()=> {
+  $("#prevPhoto").on('click', () => {
     var lastImage = mImages.length;
     //
     // if (mCurrentIndex == 0){
@@ -147,20 +160,18 @@ $("#prevPhoto").on('click', ()=> {
     //   console.log("index: " + lastImage);
     // }
 
-  // mCurrentIndex = mCurrentIndex - 1;
+    // mCurrentIndex = mCurrentIndex - 1;
 
+    mCurrentIndex -= 2;
+    swapPhoto();
 
-  mCurrentIndex -= 2;
-  swapPhoto();
-
-})
-
+  })
 
 });
 
 window.addEventListener('load', function() {
 
-	console.log('window loaded');
+  console.log('window loaded');
 
 }, false);
 
@@ -171,18 +182,3 @@ function GalleryImage(imgLocation, imgInfo, imgDate, imgURL) {
   this.date = imgDate;
   this.imgPath = imgURL;
 }
-
-function getQueryParams(qs) {
-  qs = qs.split("+").join(" ");
-  var params = {},
-   tokens,
-   re = /[?&]?([^=]+)=([^&]*)/g;
-   while (tokens = re.exec(qs)) {
-     params[decodeURIComponent(tokens[1])]
-     = decodeURIComponent(tokens[2]);
-   }
-   return params;
- }
-
-var $_GET = getQueryParams(document.location.search);
-console.log($_GET["json"]); // would output "John"
